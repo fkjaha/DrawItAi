1ea2c69ce65c0c397598ba7cb252d7d989a60ddc
fkjaha <fkjaha@gmail.com>
2025-01-07 04:14:06 +0100

    6.3

---
+++ DrawIt/Assets/Scripts/DevTools/AutoRectAnchorsPlacer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AutoRectAnchorsPlacer : MonoBehaviour
+{
+    [SerializeField] private RectTransform rectTransform;
+
+    [ContextMenu("Auto Anchors")]
+    public void AutoAnchors()
+    {
+        RectTransform parentRectTransform = rectTransform.parent.GetComponent<RectTransform>();
+        if (parentRectTransform == null) return;
+        
+        float parentWidth = parentRectTransform.rect.width;
+        float parentHeight = parentRectTransform.rect.height;
+        
+        Vector2 anchorMaxChange = Vector2.zero;
+        Vector2 anchorMinChange = Vector2.zero;
+        
+        anchorMaxChange.x = rectTransform.offsetMax.x / parentWidth;
+        anchorMaxChange.y = rectTransform.offsetMax.y / parentHeight;
+        anchorMinChange.x = rectTransform.offsetMin.x / parentWidth;
+        anchorMinChange.y = rectTransform.offsetMin.y / parentHeight;
+        
+        rectTransform.anchorMax += anchorMaxChange;
+        rectTransform.anchorMin += anchorMinChange;
+        rectTransform.offsetMax = Vector2.zero;
+        rectTransform.offsetMin = Vector2.zero;
+    }
+}
---
+++ DrawIt/Assets/Scripts/Editor/MoreEditorShortcuts/UiShortcuts.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MoreEditorShortcuts
+{
+    public class UiShortcuts
+    {
+        [MenuItem("EditorUtils/Ui/Set Anchors to rect corners  &s")]
+        public static void TogglePlaymode()
+        {
+            RectTransform rectTransform = Selection.activeGameObject.GetComponent<RectTransform>();
+            if (rectTransform == null) return;
+            
+            RectTransform parentRectTransform = rectTransform.parent.GetComponent<RectTransform>();
+            if (parentRectTransform == null) return;
+        
+            Undo.RecordObject(rectTransform, "Set Anchors to rect corners");
+            
+            float parentWidth = parentRectTransform.rect.width;
+            float parentHeight = parentRectTransform.rect.height;
+        
+            Vector2 anchorMaxChange = Vector2.zero;
+            Vector2 anchorMinChange = Vector2.zero;
+        
+            anchorMaxChange.x = rectTransform.offsetMax.x / parentWidth;
+            anchorMaxChange.y = rectTransform.offsetMax.y / parentHeight;
+            anchorMinChange.x = rectTransform.offsetMin.x / parentWidth;
+            anchorMinChange.y = rectTransform.offsetMin.y / parentHeight;
+        
+            rectTransform.anchorMax += anchorMaxChange;
+            rectTransform.anchorMin += anchorMinChange;
+            rectTransform.offsetMax = Vector2.zero;
+            rectTransform.offsetMin = Vector2.zero;
+        }
+    }
+}
